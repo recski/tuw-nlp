ed588c248f5d3005144091358dd425de887558bb
Adam Kovacs <adaamko@nessie.ilab.sztaki.hu>
2020-10-14 20:24:52 +0200

    Added default fourlang parser

--- test/grammar/test_ud_fl.py
+++ test/grammar/test_ud_fl.py
@@ -1,6 +1,8 @@
+import stanza
 from tuw_nlp.text.segmentation import CustomStanzaPipeline
-from tuw_nlp.grammar.ud_fl import UD_FL
+from tuw_nlp.grammar.ud_fl import UD_FL, UD_Fourlang
 
+en_nlp = stanza.Pipeline('en')
 
 def test_ud_fl():
     text = 'Dachgeschosse sind nicht zulässig.'
@@ -9,3 +11,15 @@ def test_ud_fl():
     sen = nlp(text).sentences[0]
     fl = ud_fl.parse(sen, 'ud', 'fl', 'amr-sgraph-src')
     assert fl == "(u_1<root> / PER  :1 (u_3 / Dachgeschosse))"
+
+def test_ud_fourlang_en():
+    text = 'I have a dog.'
+    ud_fl = UD_Fourlang()
+    nlp = stanza.Pipeline('en')
+    sen = nlp(text).sentences[0]
+    fl = ud_fl.parse(sen, 'ud', 'fourlang', 'amr-sgraph-src')
+    assert fl == "(u_1<root> / PER  :1 (u_3 / Dachgeschosse))"
+
+
+if __name__ == "__main__":
+    test_ud_fourlang_en()
--- tuw_nlp/grammar/lexicon.py
+++ tuw_nlp/grammar/lexicon.py
@@ -41,7 +41,7 @@ class IRTGRuleLexicon():
             ("VERB", "AUX", "AUX"),
             # liegen -> Baulinie, sample 6
             ("VERB", "OBL", "NOUN"),
-            }
+        }
 
         self.mod_edges |= {
             (pos1, dep, pos2)
@@ -104,7 +104,7 @@ class FSLexicon(IRTGRuleLexicon):
             ("VERB", "CONJ", "VERB"): [
                 'unify(emb_coord1(?1), emb_coord2(?2))'],
             ("NOUN", "CASE", "ADP"): ["emb_prop(unify(?1, emb_mod(?2)))"]
-            }
+        }
 
         self.bin_fnc.update({
             edge: ["emb_prop(unify(emb_mod(?1), ?2))"]
@@ -169,7 +169,7 @@ class CFLLexicon(IRTGRuleLexicon):
             ("VERB", "NSUBJ", "PRON"): [r("1")],
             ("VERB", "CONJ", "VERB"): [coord],
             ("NOUN", "CASE", "ADP"): [r("0")]
-            }
+        }
 
         self.bin_fnc.update({edge: [r("0")] for edge in self.mod_edges})
 
@@ -217,3 +217,133 @@ class CFLLexicon(IRTGRuleLexicon):
 
     def get_default_terminal(self, word):
         return f'"({word}<root> / {word})"'
+
+
+class DefaultLexicon():
+    def __init__(self):
+        self.default_binary_rule = "?1"
+        self.bin_fnc = {}
+        self.relation_terms = set()
+
+        def r_out(ud_edge, fourlang_edge):
+            return f'f_dep1(merge(merge(?1,"(r<root> :{ud_edge} (d1<dep1>))"), r_dep1(?2)))', f'f_dep1(merge(merge(?1,"(r<root> :{fourlang_edge} (d1<dep1>))"), r_dep1(?2)))'
+
+        def r_in(ud_edge, fourlang_edge):
+            return f'f_dep1(merge(merge(?1,"(r<root> :{ud_edge} (d1<dep1>))"), r_dep1(?2)))', f'f_dep1(merge(merge(?1,"(d1<dep1> :{fourlang_edge} (r<root>))"), r_dep1(?2)))'
+
+        def r_relation(ud_edge, relation):
+            return f'f_dep1(merge(merge(?1,"(r<root> :{ud_edge} (d1<dep1>))"), r_dep1(?2)))', f'f_dep1(merge(merge(?1,"({relation}<relation> / {relation} :1 (r<root>) :2 (d1<dep1>)))"), r_dep1(?2)))'
+
+        def r_in_out(ud_edge, fourlang_out, fourlang_in):
+            return f'f_dep1(merge(merge(?1,"(r<root> :{ud_edge} (d1<dep1>))"), r_dep1(?2)))', f'f_dep1(merge(merge(?1,"(r<root> :{fourlang_out} (d1<dep1> :{fourlang_in} (r<root>)))"), r_dep1(?2)))'
+
+        def r_default_binary_rule(ud_edge):
+            return f'f_dep1(merge(merge(?1,"(r<root> :{ud_edge} (d1<dep1>))"), r_dep1(?2)))', f'?1'
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "sorted_train_edges_mapped"), "r+") as f:
+            for line in f:
+                line = line.strip("\n")
+                if line:
+                    line = line.split("\t")
+                    pos = line[0]
+                    dep = line[2].replace(":", "_").upper()
+                    cpos = line[1]
+                    out_edge = None if line[4] == "-" else line[4]
+                    in_edge = None if line[5] == "-" else line[5]
+
+                    if in_edge and not out_edge:
+                        self.bin_fnc[(pos, dep, cpos)] = r_in(dep, in_edge)
+                    elif not in_edge and out_edge:
+                        self.bin_fnc[(pos, dep, cpos)] = r_out(dep, out_edge)
+                    elif in_edge and out_edge:
+                        self.bin_fnc[(pos, dep, cpos)] = r_in_out(
+                            dep, in_edge, out_edge)
+                    elif not in_edge and not out_edge and len(line) == 7:
+                        self.relation_terms.add(line[6])
+                        self.bin_fnc[(pos, dep, cpos)] = r_relation(
+                            dep, line[6])
+                    else:
+                        self.bin_fnc[(pos, dep, cpos)
+                                     ] = r_default_binary_rule(dep)
+
+    def get_dependency_rules(self, pos, dep, cpos):
+        return [self.bin_fnc.get((pos, dep, cpos))]
+
+    def get_default_terminal(self, word, i):
+        return f'"({i}<root> / {word})"', f'"({word}<root> / {word})"'
+
+    def get_relation_terminal(self, word):
+        return f'"({word}<relation> / {word})"', f'"({word}<relation> / {word})"'
+
+    def handle_conj(self, parent_pos, parent_dep, current_pos, children_pos, i):
+        coordination = (
+            f"{parent_pos} -> coordination_{i}(SUBGRAPHNODE, COORD)",
+            {f'[ud] f_dep1(merge(merge(?1,"(r<root> :{parent_dep} (d1<dep1>))"), r_dep1(?2)))',
+             f'[fourlang] r_coord_root(merge(?1, ?2))'},
+            "nonterminal"
+        )
+
+        handle_coord = (
+            f"COORD -> handle_coord_{i}(COORD, {children_pos})",
+            f'[ud] f_dep1(merge(merge(?1,"(r<root> :CONJ (d1<dep1>))"), r_dep1(?2)))',
+            f'[fourlang] f_dep1(merge(merge(?1,"(r<coord> :2 (d1<dep1>))"), r_dep1(?2)))',
+            ""
+        )
+
+        coord_to_pos = (
+            f"COORD -> coord_to_{current_pos}{i}({current_pos})",
+            {f'[ud] ?1',
+             f'[fourlang] f_dep1(merge("(r<coord> :2 (d1<dep1>))", r_dep1(?1)))'},
+            "nonterminal"
+        )
+
+        subgraph_node = (
+            f"SUBGRAPHNODE -> subgraph_to_node{i}({parent_pos})",
+            {f"[ud] ?1",
+             f'[fourlang] r_coord(?1)'},
+            "nonterminal"
+        )
+
+        return [coordination, handle_coord, coord_to_pos, subgraph_node]
+
+    def handle_acl_relcl(self, parent_pos, current_pos, children_pos, i):
+        acl_relcl = (
+            f"{parent_pos} -> handle_acl_relcl{i}({parent_pos}, {current_pos}, {children_pos})",
+            {f'[ud] f_dep2(merge(merge(?1,"(r<root> :ACL_RELCL (d2<dep2>))"), r_dep2(f_dep1(merge(merge(?2,"(r<root> :NSUBJ (d1<dep1>))"), r_dep1(?3))))))',
+             f'[fourlang] f_dep1(merge(merge(?1,"(r<root> :0 (d1<dep1>))"), r_dep1(?2)))'},
+            "nonterminal"
+        )
+
+        return [acl_relcl]
+
+    def handle_obl_case(self, parent_pos, current_pos, children_pos, i):
+        obl_case = (
+            f"{parent_pos} -> handle_obl_case{i}({parent_pos}, {current_pos}, {children_pos})",
+            {f'[ud] f_dep2(merge(merge(?1,"(r<root> :OBL (d2<dep2>))"), r_dep2(f_dep1(merge(merge(?2,"(r<root> :CASE (d1<dep1>))"), r_dep1(?3))))))',
+             f'[fourlang] f_dep2(f_dep1(merge(merge(merge(?1,"(d1<dep1> :1 (r<root>) :2 (d2<dep2>))"), r_dep1(?3)), r_dep2(?2))))'},
+            "nonterminal"
+        )
+
+        return [obl_case]
+
+    def handle_subgraphs(self, lemma, pos, clemma, cpos, dep, parent, i):
+        parent_dep = parent[2]
+        parent_pos = parent[1]
+
+        subgraph = None
+
+        if dep == "CONJ":
+            return self.handle_conj(parent_pos, parent_dep, pos, cpos, i)
+
+        if parent_dep == "ACL_RELCL":
+            return self.handle_acl_relcl(parent_pos, pos, cpos, i)
+
+        if parent_dep == "OBL":
+            return self.handle_obl_case(parent_pos, pos, cpos, i)
+
+        return subgraph
+
+    def get_terminal_rules(self, word, pos, i):
+        terminal_fss = self.get_default_terminal(word, i)
+
+        return [terminal_fss]

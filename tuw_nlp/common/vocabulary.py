280804be35a6694f9fcc2dd789686059a4185193
Gabor Recski <gabor.recski@tuwien.ac.at>
2021-03-03 11:34:51 +0100

    simple vocab utility

---
+++ tuw_nlp/common/vocabulary.py
@@ -0,0 +1,29 @@
+
+class Vocabulary():
+    def __init__(self):
+        self.id_to_word = {}
+        self.word_to_id = {}
+        self.next_id = 0
+
+    def __len__(self):
+        return self.next_id
+
+    def add(self, word, fail_if_exists=False):
+        if word in self.word_to_id:
+            if fail_if_exists:
+                raise ValueError(
+                    f'{word} already added and fail_if_exists set to True')
+        else:
+            self.id_to_word[self.next_id] = word
+            self.word_to_id[word] = self.next_id
+            self.next_id += 1
+
+    def get_id(self, word, allow_new=True):
+        if allow_new is False and word not in self.word_to_id:
+            raise ValueError(
+                f'{word} not in vocab and allow_new set to False')
+        self.add(word)
+        return self.word_to_id[word]
+
+    def get_word(self, word_id):
+        return self.id_to_word[word_id]

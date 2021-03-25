ab9b2020ced5bdf9aca872a18af482706b334ea5
Adam Kovacs <adaam.ko@gmail.com>
2021-03-25 11:59:17 +0100

    frequencies in the vocabulary

--- tuw_nlp/common/vocabulary.py
+++ tuw_nlp/common/vocabulary.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
+from heapq import nlargest
+
 
 class Vocabulary():
     def __init__(self):
         self.id_to_word = {}
         self.word_to_id = {}
+        self.word_to_freq = defaultdict(int)
         self.next_id = 0
 
     def __len__(self):
@@ -23,7 +27,19 @@ class Vocabulary():
             raise ValueError(
                 f'{word} not in vocab and allow_new set to False')
         self.add(word)
+        self.word_to_freq[word] += 1
         return self.word_to_id[word]
 
     def get_word(self, word_id):
         return self.id_to_word[word_id]
+
+    def select_n_best(self, max_features):
+        res_largest = nlargest(
+            max_features, self.word_to_freq, key=self.word_to_freq.get)
+
+        relabeled_indices = {}
+
+        for i, top in enumerate(res_largest):
+            relabeled_indices[self.word_to_id[top]] = i
+
+        return relabeled_indices, len(res_largest)

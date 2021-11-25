cca0ad04ee6b956cf032c1fadd52bd68b56fdaec
Adam Kovacs <adaam.ko@gmail.com>
2021-11-25 13:51:27 +0100

    setup changes

--- setup.py
+++ setup.py
@@ -40,8 +40,14 @@ setup(
         "graphviz"
     ],
     packages=find_packages(),
-    scripts=['setup.sh'],
     include_package_data=True,
     cmdclass={'develop': SetupAltoDevelop,
               'install': SetupAltoInstall, "egg_info": SetupAltoEgg},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
     zip_safe=False)

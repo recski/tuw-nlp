6f899ad4538da61802acabe4d715331ffc31e5b9
Kinga Gémes <kinga.andrea.gemes@gmail.com>
2022-04-20 13:07:08 +0200

    Revert "Simple solution for penman representation of disconnected graphs"

--- tuw_nlp/graph/utils.py
+++ tuw_nlp/graph/utils.py
@@ -484,34 +484,35 @@ def pn_to_graph(raw_dl, edge_attr="color"):
 
 
 def graph_to_pn(graph):
-    sub_graphs = [nx.subgraph(graph, sub) for sub in nx.weakly_connected_components(graph)]
-    pn_repr = ""
-    for sub_graph in sub_graphs:
-        nodes = {}
-        pn_edges, pn_nodes = [], []
+    nodes = {}
+    pn_edges, pn_nodes = [], []
 
-        for u, v, e in sub_graph.edges(data=True):
-            for node in u, v:
-                if node not in nodes:
-                    name = Graph.d_clean(str(sub_graph.nodes[node]['name']))
-                    pn_id = f'u_{node}'
-                    nodes[node] = (pn_id, name)
-                    pn_nodes.append((pn_id, ':instance', name))
-
-            pn_edges.append((nodes[u][0], f':{e["color"]}', nodes[v][0]))
-
-        for node in sub_graph.nodes():
+    for u, v, e in graph.edges(data=True):
+        for node in u, v:
             if node not in nodes:
-                name = str(sub_graph.nodes[node]['name'])
+                name = graph.nodes[node]['name']
                 pn_id = f'u_{node}'
                 nodes[node] = (pn_id, name)
                 pn_nodes.append((pn_id, ":instance", name))
 
-        G = pn.Graph(pn_nodes + pn_edges)
+        pn_edges.append((nodes[u][0], f':{e["color"]}', nodes[v][0]))
 
-        # two spaces before edge name, because alto does it :)
-        pn_repr = "\n".join([pn_repr, pn.encode(G, indent=0).replace('\n', '  ')])
-    return pn_repr
+    for node in graph.nodes():
+        if node not in nodes:
+            name = graph.nodes[node]['name']
+            pn_id = f'u_{node}'
+            nodes[node] = (pn_id, name)
+            pn_nodes.append((pn_id, ':instance', name))
+
+    G = pn.Graph(pn_nodes + pn_edges)
+
+    try:
+    # two spaces before edge name, because alto does it :)
+        return pn.encode(G, indent=0).replace('\n', '  ')
+    except pn.exceptions.LayoutError as e:
+        words = [graph.nodes[node]['name'] for node in graph.nodes()]
+        logging.error(f'pn.encode failed on this graph: {words}')
+        raise e
 
 
 def read_alto_output(raw_dl):

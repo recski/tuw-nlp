9be4e995ae7df63584f24bc21d849570c6f5e794
Martin Bär <e1500768@student.tuwien.ac.at>
2022-06-13 10:36:38 +0200

    replace 'im' (and similar) now with 'i am'

--- tuw_nlp/text/preprocess/hatexplain.py
+++ tuw_nlp/text/preprocess/hatexplain.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 def regularize_im(tweet):
-    tweet = re.sub(r"\bi[ ]*['´`’]*[ ]*m\b", "i'm", tweet, flags=re.I)
+    tweet = re.sub(r"\bi[ ]*['´`’]*[ ]*m\b", "i am", tweet, flags=re.I)
     return tweet
 
 def preprocess_hatexplain(tweet):
@@ -10,16 +10,6 @@ def preprocess_hatexplain(tweet):
     return tweet
 
 def main():
-    """
-    test = ["I ' m super gay fuck",
-        "im impatient im awesome i'm gay",
-        "impatient I am",
-        "m gay",
-        "i ’ m super gay fuck",
-        "im gay i love fathers dm for dilfs",
-        "when i say i only like seven men i mean i only love seven men bc im fucking gay",
-        "yeah im def gay"]
-    """
     for line in sys.stdin:
         print(preprocess_hatexplain(line.strip()))
 

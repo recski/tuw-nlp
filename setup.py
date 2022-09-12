97273c2e78e4708aed2db5afe7ff630c00988c6f
Adam Kovacs <adaam.ko@gmail.com>
2022-09-12 14:24:07 +0200

    Publish v0.0.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class SetupAltoEgg(egg_info):
 
 setup(
     name="tuw-nlp",
-    version="0.0.8",
+    version="0.0.9",
     description="NLP tools at TUW Informatics",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",

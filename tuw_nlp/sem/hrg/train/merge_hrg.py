6c539e36f45271821f22f2e6c11bba51c045d324
Eszti <eszter.iklodi@gmail.com>
2024-02-18 11:45:35 +0100

    Add size arg for merge_hrg

--- tuw_nlp/sem/hrg/train/merge_hrg.py
+++ tuw_nlp/sem/hrg/train/merge_hrg.py
@@ -1,19 +1,18 @@
 import argparse
 import os
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-i", "--input-dir", type=str)
     parser.add_argument("-o", "--out-dir", default="data", type=str)
+    parser.add_argument("-s", "--size", type=int)
     return parser.parse_args()
 
 
-def main(input_dir, out_dir):
-    initial_rules = Counter()
-    rules = Counter()
-    nts = Counter()
+def main(input_dir, out_dir, size):
+    grammar = defaultdict(Counter)
     for sen_dir in os.listdir(input_dir):
         filename = os.path.join(input_dir, sen_dir, f"sen{sen_dir}.hrg")
         if os.path.exists(filename):
@@ -22,33 +21,45 @@ def main(input_dir, out_dir):
             for line in lines:
                 rule = line.strip()
                 nt = rule[0]
-                if nt == "S":
-                    initial_rules[rule] += 1
-                else:
-                    rules[rule] += 1
-                nts[nt] += 1
+                grammar[nt][rule] += 1
     with open(os.path.join(out_dir, "grammar.hrg"), "w") as f:
-        write_rule(f, initial_rules, "weight", nts)
-        write_rule(f, rules, "weight", nts)
+        write_rule(f, grammar, "weight", size)
     with open(os.path.join(out_dir, "grammar_stat"), "w") as f:
-        write_rule(f, initial_rules, "cnt")
-        write_rule(f, rules, "cnt")
+        write_rule(f, grammar, "cnt", size)
 
 
-def write_rule(f, rules, numeric_info=None, nts=None):
-    for rule, cnt in rules.most_common():
+def write_rule(f, grammar, numeric_info=None, size=None):
+    factor = 1
+    if size:
+        all_prods = 0
+        for nt, prods in grammar.items():
+            all_prods += len(prods)
+        factor = size / all_prods
+    for prod, cnt in grammar["S"].most_common(n=int(round(factor*len(grammar["S"])))):
         if not numeric_info:
-            f.write(f"{rule}\n")
+            f.write(f"{prod}\n")
         elif numeric_info == "cnt":
-            f.write(f"{rule}\t{cnt}\n")
-        elif numeric_info == "weight" and nts:
-            nt = rule[0]
-            w = round(cnt / nts[nt], 2)
+            f.write(f"{prod}\t{cnt}\n")
+        elif numeric_info == "weight":
+            w = round(cnt / grammar["S"].total(), 2)
             if w < 0.01:
                 w = 0.01
-            f.write(f"{rule}\t{w}\n")
+            f.write(f"{prod}\t{w}\n")
+    for nt, prods in grammar.items():
+        if nt == "S":
+            continue
+        for prod, cnt in prods.most_common(n=int(round(factor*len(prods)))):
+            if not numeric_info:
+                f.write(f"{prod}\n")
+            elif numeric_info == "cnt":
+                f.write(f"{prod}\t{cnt}\n")
+            elif numeric_info == "weight":
+                w = round(cnt / prods.total(), 2)
+                if w < 0.01:
+                    w = 0.01
+                f.write(f"{prod}\t{w}\n")
 
 
 if __name__ == "__main__":
     args = get_args()
-    main(args.input_dir, args.out_dir)
+    main(args.input_dir, args.out_dir, args.size)
